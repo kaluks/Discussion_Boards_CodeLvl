e4229701187263e4c87119c9bccbfe3fd72f006c
Kathryn <kanne1126@gmail.com>
2017-11-13 19:33:37 -0500

    edit button

--- boards/views.py
+++ boards/views.py
@@ -4,6 +4,10 @@ from .forms import NewTopicForm, PostForm
 from .models import Discussion_Board, Discussion, Post
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.views.generic import View, CreateView, UpdateView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+
 
 def home(request):
     boards = Discussion_Board.objects.all()
@@ -56,3 +60,33 @@ def topic_posts(request, pk, topic_pk):
     topic.views += 1
     topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
+
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
